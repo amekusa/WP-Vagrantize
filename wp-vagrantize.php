c6e7f1a9d4c3122abb9c51e4d0a988368c115999
amekusa <post@amekusa.com>
2015-03-05 14:51:45 +0900

    Work around the entry point

--- wp-vagrantize.php
+++ wp-vagrantize.php
@@ -1,18 +1,12 @@
 <?php
-namespace amekusa\WPVagrantize;
-
 /*
-Plugin Name: WP Vagrantize
-Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
-Plugin URI: PLUGIN SITE HERE
-Text Domain: wp-vagrantize
-Domain Path: /languages
-*/
-
-add_action('init', function () { // The entry point
-	include __DIR__.'/vendor/autoload.php';
-	
-});
+ Plugin Name: WP Vagrantize
+ Version: 0.1-alpha
+ Description: PLUGIN DESCRIPTION HERE
+ Author: YOUR NAME HERE
+ Author URI: YOUR SITE HERE
+ Plugin URI: PLUGIN SITE HERE
+ Text Domain: wp-vagrantize
+ Domain Path: /languages
+ */
+require_once __DIR__ . '/src/main.php';
