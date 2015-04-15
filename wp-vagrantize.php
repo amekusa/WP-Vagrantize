0912e04e3a99ee755f21dcb93cfe9a107a7f5d82
amekusa <post@amekusa.com>
2015-04-15 15:49:52 +0900

    + Trailing slash to WP_VAGRANTIZE_HOME

--- wp-vagrantize.php
+++ wp-vagrantize.php
@@ -9,6 +9,6 @@
  Text Domain: wp-vagrantize
  Domain Path: /languages
  */
-if (!defined('WP_VAGRANTIZE_HOME')) define('WP_VAGRANTIZE_HOME', __DIR__);
+if (!defined('WP_VAGRANTIZE_HOME')) define('WP_VAGRANTIZE_HOME', __DIR__ . '/');
 if (!defined('WP_VAGRANTIZE_URL')) define('WP_VAGRANTIZE_URL', plugin_dir_url(__FILE__));
 require_once __DIR__ . '/src/main.php';
