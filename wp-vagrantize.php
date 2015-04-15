6ff0205a4072f77f2eedcf1bca67dee63c1c89f8
amekusa <post@amekusa.com>
2015-04-15 15:40:01 +0900

    + Constant: WP_VAGRANTIZE_URL

--- wp-vagrantize.php
+++ wp-vagrantize.php
@@ -10,4 +10,5 @@
  Domain Path: /languages
  */
 if (!defined('WP_VAGRANTIZE_HOME')) define('WP_VAGRANTIZE_HOME', __DIR__);
+if (!defined('WP_VAGRANTIZE_URL')) define('WP_VAGRANTIZE_URL', plugin_dir_url(__FILE__));
 require_once __DIR__ . '/src/main.php';
