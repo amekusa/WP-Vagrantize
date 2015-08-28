db5fb79ab29d98d34e4809553a787470e4377b14
amekusa <post@amekusa.com>
2015-08-28 14:24:32 +0900

    Update the plugin header

--- wp-vagrantize.php
+++ wp-vagrantize.php
@@ -5,7 +5,7 @@
  Description: A smart way to reproduce your blog into the local machine
  Author: Satoshi Soma a.k.a amekusa
  Author URI: https://github.com/amekusa
- Plugin URI: https://github.com/amekusa/WP-Vagrantize
+ Plugin URI: https://github.com/amekusa/wp-vagrantize
  Text Domain: wp-vagrantize
  */
 if (!defined('WP_VAGRANTIZE_HOME')) define('WP_VAGRANTIZE_HOME', __DIR__ . '/');
